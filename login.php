4037263d1f581b667ff5b56bddca60274559d40a
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2020-02-15 13:17:17 +0100

    update interface

--- login.php
+++ login.php
@@ -14,9 +14,8 @@ $provider = new League\OAuth2\Client\Provider\Github([
     'redirectUri'       => 'https://transfer.lucacastelnuovo.nl/login',
 ]);
 
-function redirect($to, $alert = null)
+function redirect($to)
 {
-    $_SESSION['alert'] = $alert;
     header('location: ' . $to);
     exit;
 }
@@ -30,7 +29,7 @@ if (isset($_GET['authenticate'])) {
 
 if (isset($_GET['code'])) {
     if(empty($_GET['state']) || ($_GET['state'] !== $_SESSION['state']))  {
-        redirect('/login?reset', 'Invalid State');
+        redirect('/login?msg=invalid_state');
     }
 
     $token = $provider->getAccessToken('authorization_code', [
@@ -41,7 +40,7 @@ if (isset($_GET['code'])) {
         $username = $provider->getResourceOwner($token)->getNickname();
 
         if (!in_array($username, $allowed_users)) {
-            redirect('/login?reset', 'Account not approved');
+            redirect('/login?msg=unauthorized_user');
         }
 
         $_SESSION['logged_in'] = true;
@@ -49,76 +48,10 @@ if (isset($_GET['code'])) {
 
         redirect('/');
     } catch (Exception $error) {
-        redirect('/login?reset', $error);
+        redirect('/login?msg=unknown_error');
     }
 }
 
-if (isset($_GET['reset'])) {
-    if (isset($_SESSION['alert'])) {
-        $alert = $_SESSION['alert'];
-    }
 
-    session_destroy();
-    session_start();
-
-    redirect('/login', $alert);
-}
-
-
-if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
-    redirect('/');
-}
-
-?>
-<!DOCTYPE html>
-<html>
-
-<head>
-    <!-- Config -->
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link rel="manifest" href="manifest.json?1.3.2" />
-    <title>Login || Secure Transfer</title>
-
-    <!-- Icons -->
-    <link rel="apple-touch-icon" href="img/apple-touch-icon.png?1.3.2" sizes="180x180" />
-    <link rel="icon" type="image/png" href="img/favicon-32x32.png?1.3.2" sizes="32x32" />
-    <link rel="icon" type="image/png" href="img/favicon-16x16.png?1.3.2" sizes="16x16" />
-    <link rel="manifest" href="manifest.json?1.3.2" />
-    <link rel="mask-icon" href="img/safari-pinned-tab.svg?1.3.2" color="#ffcc00" />
-    <link rel="shortcut icon" href="img/favicon.ico">
-    <meta name="msapplication-config" content="browserconfig.xml">
-    <meta name="theme-color" content="#ffe57e" />
-
-    <!-- Styles -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-</head>
-
-<body>
-<div class="row">
-    <div class="col s12 m8 offset-m2 l4 offset-l4">
-        <div class="card">
-            <div class="card-action blue accent-4 white-text">
-                <h3>Secure Transfer</h3>
-            </div>
-            <div class="card-content">
-                <div class="row center">
-                    <a class="waves-effect waves-light btn-large blue accent-4" href="?authenticate">
-                        Login with GitHub
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <?php 
-        if (isset($_SESSION['alert']) && !empty($_SESSION['alert'])) {
-            echo "<script>M.toast({html: \"{$_SESSION['alert']}\"});</script>";
-            unset($_SESSION['alert']);
-        }
-    ?>
-</body>
-
-</html>
+session_destroy();
+redirect("/?msg={$_GET['msg']}");
