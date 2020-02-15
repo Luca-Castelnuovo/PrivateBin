4259c9d9c86679285e7ccb8f09341c910eb11b87
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2020-02-15 12:39:05 +0100

    only require authentiocation for creation  not for viewing

--- login.php
+++ login.php
@@ -118,7 +118,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
             echo "<script>M.toast({html: \"{$_SESSION['alert']}\"});</script>";
             unset($_SESSION['alert']);
         }
-    ?>>
+    ?>
 </body>
 
 </html>
--- tpl/bootstrap.php
+++ tpl/bootstrap.php
@@ -4,14 +4,6 @@ $isCpct = substr($template, 9, 8) === '-compact';
 $isDark = substr($template, 9, 5) === '-dark';
 $isPage = substr($template, -5) === '-page';
 
-session_start();
-if ((!$_SESSION['logged_in']) || ($_SESSION['ip'] != $_SERVER['REMOTE_ADDR']) || (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800))) {
-	header("Location: /login?reset");
-} else {
-	$_SESSION['LAST_ACTIVITY'] = time();
-}
-
-
 ?><!DOCTYPE html>
 <html lang="<?php echo I18n::_('en'); ?>">
 	<head>
@@ -415,7 +407,7 @@ endif;
 				<ul class="nav navbar-nav pull-right">
 					<li>
 						<a href="/login?reset" id="logoutbutton" type="button" class=" btn btn-<?php echo $isDark ? 'warning' : 'default'; ?> navbar-btn">
-							<span class="glyphicon glyphicon-user" aria-hidden="true"></span> Logout
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span> Login/Logout
 						</a>
 					</li>
 <?php
